1d191b8476dde0381adbe696b37f95d8c63c1820
Alexander <aleksievisa@gmail.com>
2023-10-30 20:41:24 +0300

    test

--- app/api/avatar/upload/route.ts
+++ app/api/avatar/upload/route.ts
@@ -1,23 +1,29 @@
 import { put } from '@vercel/blob';
 import { NextResponse } from 'next/server';
+import axios from 'axios'
+
+const url = "https://prod-files-secure.s3.us-west-2.amazonaws.com/3d9f4422-d6af-4734-8208-04195a73c696/036f52bc-c9f6-40ab-a6f0-6ba7f7b2c9e8/Rectangle_9.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20231030%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20231030T173307Z&X-Amz-Expires=3600&X-Amz-Signature=ec86c68a5bb77b5ea7b3eb666e83a2ff7a209f90961d499e0e10abe6967f354e&X-Amz-SignedHeaders=host&x-id=GetObject"
+
+export async function getImageAsBase64(url: string) {
+  const response = await axios.get(url, { responseType: 'arraybuffer' });
+  const image = Buffer.from(response.data, 'binary').toString('base64');
+  return `data:image/png;base64,${image}`;
+}
 
 export async function POST(request: Request): Promise<NextResponse> {
   const { searchParams } = new URL(request.url);
   const filename = <string>searchParams.get('filename');
   // ⚠️ The below code is for App Router Route Handlers only
-  const blob = await put(filename, request.body!, {
+
+  const file = await getImageAsBase64(url)
+
+  const blob = await put(filename, file, {
     access: 'public',
   });
 
   console.log(blob)
 
-  
-  // Here's the code for Pages API Routes:
-  // const blob = await put(filename, request, {
-  //   access: 'public',
-  // });
-
-  return NextResponse.json(blob);
+  return NextResponse.json({ blob });
 }
 
 // The next lines are required for Pages API Routes only
