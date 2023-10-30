ddf35154dcd7b888d1a3a68bb8d29f45d1552b39
Alexander <aleksievisa@gmail.com>
2023-10-30 20:42:49 +0300

    test

--- app/api/avatar/upload/route.ts
+++ app/api/avatar/upload/route.ts
@@ -4,7 +4,7 @@ import axios from 'axios'
 
 const url = "https://prod-files-secure.s3.us-west-2.amazonaws.com/3d9f4422-d6af-4734-8208-04195a73c696/036f52bc-c9f6-40ab-a6f0-6ba7f7b2c9e8/Rectangle_9.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20231030%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20231030T173307Z&X-Amz-Expires=3600&X-Amz-Signature=ec86c68a5bb77b5ea7b3eb666e83a2ff7a209f90961d499e0e10abe6967f354e&X-Amz-SignedHeaders=host&x-id=GetObject"
 
-export async function getImageAsBase64(url: string) {
+async function getImageAsBase64(url: string) {
   const response = await axios.get(url, { responseType: 'arraybuffer' });
   const image = Buffer.from(response.data, 'binary').toString('base64');
   return `data:image/png;base64,${image}`;
